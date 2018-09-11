5b9696e82c90b4e3f0ea3e7aff25ddd4b3227bc4
liuzhaozhao <liuzhaozhao@jd.com>
2018-09-11 15:24:42 +0800

    admin

--- src/routes/MerchantAccount/InvestForm.js
+++ src/routes/MerchantAccount/InvestForm.js
@@ -2,7 +2,7 @@
  * Created by liuzhaozhao on 2017/12/29.
  */
 import React from 'react';
-import {Form, Input, Button, Upload, Icon} from 'antd'
+import {Form, Input, Button, Upload, Icon, message} from 'antd'
 import {connect} from "dva";
 
 const FormItem = Form.Item
@@ -117,7 +117,15 @@ class InvestForm extends React.Component {
                       action='address'
                       headers={{
                         authorization: 'authorization-text',
-                      }}>
+                      }}
+                      onChange={(info)=>{
+                        if (info.file.status === 'done') {
+                          message.success(`${info.file.name} 上传成功`);
+                        } else if (info.file.status === 'error') {
+                          message.error(`${info.file.name} 上传失败.`);
+                        }
+                        }
+                      }>
                 <Button>
                   <Icon type="upload" />
                 </Button>
